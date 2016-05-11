f8187460cc8d4d9dcf2fd87b77df5fc325fdb2f9
Matt Schlobohm <matt.schlobohm@saplinglearning.com>
2016-05-11 14:46:12 -0500

    sass-ified/

---
+++ README.md
@@ -0,0 +1,6 @@
+### Start me up!
+
+    git clone git@bitbucket.org:ron_romero_sapling/eco_frontend.git
+    cd eco_frontend
+    npm install                    # install project packages
+    npm start
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
 
-// import './less/form.less';
+require('./sass/master.scss');
 
 const initialState =  {
     auth:{
