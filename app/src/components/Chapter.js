ed4961a37fd32c7b3376f3465ad83a7879458d80
Scott Hudnall <scott.hudnall@saplinglearning.com>
2016-06-01 11:20:11 -0500

    Updates from session with Raiff on 5/27

--- app/src/components/Chapter.js
+++ app/src/components/Chapter.js
@@ -6,14 +6,14 @@ import React from 'react'
 import ChapterTitle from './ChapterTitle'
 import Assignments from './../containers/AssignmentsContainer'
 
-export default ({index, title, summary, assignments, id, isExpanded, toggleChapter}) => (
+export default ({index, title, summary, id, isExpanded, toggleChapter}) => (
         <li>
             <ChapterTitle index={index} title={title} id={id} toggleChapter={toggleChapter} />
             {isExpanded ? <div className="">
                 <p></p>
                 <p>{summary}</p>
-                <p></p> 
-                <Assignments id={id} /> 
+                <p></p>
+                <Assignments id={id} />
             </div>: null}
         </li>
     );
