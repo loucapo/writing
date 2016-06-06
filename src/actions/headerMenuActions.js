267ab886ac18419e11740df684fde612c554ddc7
Matt Schlobohm <matt.schlobohm@saplinglearning.com>
2016-06-06 14:32:48 -0500

    fixing weirdness after merge

---
+++ src/actions/headerMenuActions.js
@@ -0,0 +1,9 @@
+import {SELECT_COURSE_FROM_MENU} from './../constants';
+import { browserHistory } from 'react-router';
+
+export const onSelectCourseFromMenu = (value) => {
+  value && browserHistory.push('/course/' + value);
+  return {
+    type: SELECT_COURSE_FROM_MENU
+  }
+};
