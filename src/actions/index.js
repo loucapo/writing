94dc97fef3bbc20be84e58409e0511442ca8b8f1
Matt Schlobohm <matt.schlobohm@saplinglearning.com>
2016-06-06 14:06:24 -0500

    remove some code that doesnt make sense at the moment

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,4 +7,3 @@ export {
     expandChapter,
     getCourse
 };
-
--- src/reducers/headerMenuReducer.js
+++ src/reducers/headerMenuReducer.js
@@ -1,23 +1,9 @@
 import {SELECT_COURSE_FROM_MENU} from './../constants';
 
-const _toggleMenu = (state) => {
-    return {...state, dropdownActive: !state.dropdownActive};
-};
-
 export const headerMenuCourses = (state = {}, action = null) => {
-    switch (action.type) {
-        case SELECT_COURSE_FROM_MENU:
-            state = action.menuName === 'headerMenuCourses' ? _toggleMenu(state) : state;
-            break;
-    }
     return state;
 };
 
 export const headerMenuHelp = (state = {}, action = null) => {
-    switch (action.type) {
-        case SELECT_COURSE_FROM_MENU:
-            state = action.menuName === 'headerMenuHelp' ? _toggleMenu(state) : state;
-            break;
-    }
     return state;
 };
