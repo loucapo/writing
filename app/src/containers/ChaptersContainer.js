92d89277ba1514cd9a66c1d680511f172d80a739
Scott Hudnall <scott.hudnall@saplinglearning.com>
2016-06-08 12:39:11 -0500

    Normalize some stuff, removed extraneous comments & added passing ChaptersTester test

--- app/src/containers/ChaptersContainer.js
+++ app/src/containers/ChaptersContainer.js
@@ -1,19 +1,17 @@
-/**
- * Created by rharik on 5/11/16.
- */
-
-import { bindActionCreators } from 'redux'
+import {bindActionCreators} from 'redux'
 import {toggleChapter} from './../actions/index'
 import Chapters from './../components/Chapters.js'
 import container from './containerFactory'
 
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ toggleChapter }, dispatch)
+    return bindActionCreators({toggleChapter}, dispatch)
 }
 
-const transform = ({courses, currentCourse, chapters}) => {return {chapters:courses[currentCourse].chapters.map(chapterId => chapters[chapterId])}};
+const transform = ({courses, currentCourse, chapters}) => {
+    return {chapters: courses[currentCourse].chapters.map(chapterId => chapters[chapterId])}
+};
 
-const ChaptersContainer = container(['courses', 'currentCourse','chapters'], transform, mapDispatchToProps)(Chapters);
+const ChaptersContainer = container(['courses', 'currentCourse', 'chapters'], transform, mapDispatchToProps)(Chapters);
 
-export { transform, ChaptersContainer}
+export {transform, ChaptersContainer}
