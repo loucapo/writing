3e3712a256580a0fa36b0bcaf14368f68c2078c7
Raif Harik <raif.harik@saplinglearning.com>
2016-06-29 14:17:20 -0500

    tweakin

--- app/src/index.js
+++ app/src/index.js
@@ -2,8 +2,16 @@ var cache = require('./objectCache');
 var courseDataGenerator = require('./courseDataGenerator');
 var courseSchema = require('./schemas/courseSchemas.json');
 
+var schemas = {
+    definitions: {
+        courses: courseSchema.course,
+        section: courseSchema.section,
+        assignment: courseSchema.assignment
+    }
+};
+
 module.exports = {
     cache,
     courseDataGenerator,
-    courseSchema
+    schemas
 };
