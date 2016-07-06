058c7f42efe38e5f09fa4c1b66aeef5ca27e7ad5
Raif Harik <reharik@gmail.com>
2016-07-05 20:25:32 -0500

    trying to extend notifications

--- app/tests/joinDefinitionsTests.js
+++ app/tests/joinDefinitionsTests.js
@@ -6,11 +6,12 @@ const should = chai.should();
 
 describe('JOIN DEFINITIONS', () => {
     describe('when_calling_definitions', () => {
+        var defs;
         beforeEach(() => {
+            defs = SUT.definitions;
         });
 
         it('should_return_shcemas_for_all_included_files', () => {
-            var defs = SUT.definitions();
             defs.course.should.not.be.null;
             defs.section.should.not.be.null;
             defs.assignment.should.not.be.null;
@@ -20,8 +21,11 @@ describe('JOIN DEFINITIONS', () => {
         });
 
         it('should_return_schemas_that_are "derefd"', () => {
-            var defs = SUT.definitions();
             defs.course.properties.sections.should.be.an.object;
         });
+
+        it('should_properly_set_up_the_course_notification', () => {
+            defs.courseNotification.allOf[1].properties.payload.title.should.equal('Course');
+        })
     });
 });
