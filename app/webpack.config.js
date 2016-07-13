b2a21a0c97fc1bcc75b90832859b36f4c46b3db3
Roberto Fuentes <robertofuentes@rfuentes.local>
2016-07-13 08:27:46 -0500

    update to support postcss; boyscout

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,72 +1,95 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const validate = require('webpack-validator');
 
-const config = {
-    // Gives you sourcemaps without slowing down rebundling
-    devtool: 'cheap-module-eval-source-map',
-    resolve: { alias: {} },
-    entry: path.join(__dirname, '/src/index.js'),
-    output: {
-        path: path.join(__dirname, '/dist/'),
-        filename: 'bundle.js',
-        publicPath: '/'
-    },
-    module: {
-        noParse: [],
-        loaders: [
-            { test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    "presets": [
-                        "es2015",
-                        "react",
-                        "stage-0"
-                    ],
-                    "env": {
-                        "development": {
-                            "plugins": [["react-transform", {
-                                "transforms": [{
-                                    "transform": "react-transform-hmr",
-                                    "imports": ["react"], "locals": ["module"]
-                                }]
-                            }]],
-                            "ignore": ["node_module", "src/sass/image"]
-                        }
-                    }
-                }},
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-            { test: /\.json?$/, loader: 'json' },
-            // { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
-            { test: /\.png$/, loader: 'url-loader', query: { mimetype: 'image/png' } },
-            { test: /\.jpg$/, loader: 'url-loader', query: { mimetype: 'image/jpg' } },
-            { test: /\.gif$/, loader: 'url-loader', query: { mimetype: 'image/gif' } },
-            { test: /\.scss$/, loaders: ['style', 'css', 'sass'] }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.tmpl.html',
-            favicon: 'app/src/sass/image/favicon.png'
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ],
-    devServer: {
-        contentBase: './dist',
-        colors: true,
-        historyApiFallback: true,
-        inline: true,
-        hot: true,
-        host: '0.0.0.0'
-    },
-    sassLoader: {
-        includePaths: [path.resolve(__dirname, './sass')]
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+const postcssImport = require('postcss-import');
+
+const webpackConfig = {
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+    host: '0.0.0.0',
+    stats: {
+      colors: true,
+      chunks: false,
+      errors: true
     }
+  },
+  devtool: 'cheap-module-eval-source-map',
+
+  entry: path.join(__dirname, '/src/index.js'),
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+
+  module: {
+    noParse: [],
+    loaders: [
+      { test: /\.jsx?$/,
+          include: path.resolve(__dirname, 'src'),
+          loader: 'babel-loader',
+          query: {
+            presets: [
+              'es2015',
+              'react',
+              'stage-0'
+            ],
+            env: {
+              development: {
+                plugins: [['react-transform', {
+                  transforms: [{
+                    transform: 'react-transform-hmr',
+                    imports: ['react'],
+                    locals: ['module']
+                  }]
+                }]],
+                ignore: ['node_module', 'src/sass/image']
+              }
+            }
+          }},
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, 'src/sass'),
+        // loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[local]!postcss-loader')
+      },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.json?$/, loader: 'json' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.png$/, loader: 'url-loader', query: { mimetype: 'image/png' } }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.tmpl.html'
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('bundle.css', { allChunks: true })
+  ],
+
+  postcss() {
+    return [
+      postcssImport({
+        addDependencyTo: webpack
+      }),
+      precss,
+      autoprefixer
+    ];
+  },
+
+  resolve: { alias: {} }
 };
 
-module.exports = config;
+module.exports = validate(webpackConfig);
