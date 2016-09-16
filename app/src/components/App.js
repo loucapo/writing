75eb03148b626555e6f03d9a94d6d3cccdb7b328
Owen Corpening <owen.corpening.contractor@saplinglearning.com>
2016-09-16 10:19:32 -0500

    removed commented out code

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-//import HelloContainer from '../containers/HelloContainer';
 import ActivityContainer from '../containers/ActivityContainer';
 
-//export default () => (<HelloContainer />);
 export default () => (<ActivityContainer />);
 
