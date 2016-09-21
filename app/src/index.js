9793e4f7ac6d95cc24aa25c2835348d4b53a41b4
William Sullivan <bill.sullivan@saplinglearning.com>
2016-09-21 09:07:53 -0500

    Introduced react-router and a pagenotfound component to use in the routing. Previous functionality has been maintained.

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router';
 import Root from './containers/Root';
 import configureStore from './store/configureStore';
 
@@ -13,6 +14,20 @@ const RootWrapper = React.createClass({
   }
 });
 
-render(<RootWrapper />,
-  document.getElementById('root')
-);
+const PageNotFound = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Page Not Found.</h1>
+        <p>Go to <Link to="/">Home Page</Link></p>
+      </div>
+    );
+  }
+});
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={RootWrapper} />
+    <Route path="*" component={PageNotFound} />
+  </Router>
+), document.getElementById('root'));
