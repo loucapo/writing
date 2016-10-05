8ba310dfb643402c2f46a01d5695d183ae446b2b
Owen Corpening <owen.corpening.contractor@saplinglearning.com>
2016-10-05 12:44:43 -0500

    WRITE-233: eslint fixes

--- app/src/components/DraftSection/DraftList/DraftItem/DraftItemDescription/DraftItemDescription.js
+++ app/src/components/DraftSection/DraftList/DraftItem/DraftItemDescription/DraftItemDescription.js
@@ -13,14 +13,20 @@ const DraftItemDescription = ({details}) => {
     <div data-id="draft-item-description" className={ draftItemDescription.left }>
       { details.learningObjectives
         ? <div className={draftItem.summaryContainer}><div>
-          <span data-id="draft-item-learning-objectives" className={draftItem.summaryLabel}>Learning Objectives</span>
+          <span
+            data-id="draft-item-learning-objectives"
+            className={draftItem.summaryLabel}>Learning Objectives
+          </span>
           { learningObjectives.map(fbt => <div key={uuid.v4()}>{fbt}</div>) }
         </div></div> : null
       }
 
       { details.peerReviewGroups
         ? <div className={draftItem.summaryContainer}><div>
-          <span data-id="draft-item-peer-review" className={draftItem.summaryLabel}>Post Instructor Feedback Survey Prompt</span>
+          <span
+            data-id="draft-item-peer-review"
+            className={draftItem.summaryLabel}>Post Instructor Feedback Survey Prompt
+          </span>
           { [details.peerReviewGroups].map(fbt => <div key={uuid.v4()}>{fbt}</div>) }
         </div></div> : null
       }
