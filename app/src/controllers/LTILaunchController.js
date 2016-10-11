e0f82fa91463539fa381b1345182c899a7449085
Raif Harik <reharik@gmail.com>
2016-10-11 15:23:49 -0500

    removing cruft and commented out code

--- app/src/controllers/LTILaunchController.js
+++ app/src/controllers/LTILaunchController.js
@@ -3,13 +3,6 @@ module.exports = function (koasend) {
   return {
     async launch(ctx) {
       
-      console.log('==========ctx.isAuthenticated()=========');
-      console.log(ctx.isAuthenticated());
-      console.log(ctx.request.user);
-      console.log(ctx.session);
-
-      console.log('==========END ctx.isAuthenticated()=========');
-      // ctx.status = 200;
       ctx.redirect('/');
     }
   }
--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -1,24 +1,7 @@
 module.exports = function (koasend) {
   return {
     async index(ctx) {
-      // var row = await repository.row('select * from "writer_key"');
-      // ctx.status = 200;
-      // ctx.body = {
-      //   status: ctx.status,
-      //   success: true,
-      //   data: row
-      // };
-
-
-
-      console.log('==========ctx.isAuthessssssssssssssssssssssssnticated()=========');
-      console.log(ctx.isAuthenticated());
-      console.log(ctx.request.user);
-      console.log(ctx.session);
-      console.log('==========END ctx.isAuthenticated()=========');
-
       await koasend(ctx, 'app/src/views/index.html');
-
     }
   };
 };
