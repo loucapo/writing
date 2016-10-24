f20f7dac5dc83c1cf7c612ae21fdf3492b3a37a6
William Sullivan <bill.sullivan@saplinglearning.com>
2016-10-24 16:22:10 -0500

    Removing console log

--- app/index.js
+++ app/index.js
@@ -3,7 +3,4 @@ require('babel-register');
 
 const data = require('./src/index').generateDB;
 //horrible @#$@## hack
-console.log('=========="running data"=========');
-console.log(data);
-console.log('==========END "running data"=========');
 setTimeout( data(), 5000);
