e89a7043f99cd51861eb94a8ec5cca8475fbcef8
Albalenys <albalenysd@gmail.com>
2016-11-11 11:26:42 -0500

    add quickFeedbackTool component provided by Owen

--- app/src/components/FeedbackTool/FeedbackTool.js
+++ app/src/components/FeedbackTool/FeedbackTool.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import RichTextEditor from 'react-rte';
+import SideMenu from './SideMenu/SideMenu';
+import feedbackTool from './feedbackTool.css';
 
-const FeedbackTool = ({document}) => {
-  return (<RichTextEditor value={document} readOnly={true}/>)
+const FeedbackTool = ({document, showQuickFeedbackTool, toggleQuickFeedback}) => {
+  return (
+    <section>
+      <div className={feedbackTool.editorContainer}>
+        <RichTextEditor value={document} readOnly={true} />
+      </div>
+      <SideMenu
+        toggleQuickFeedback={toggleQuickFeedback}
+        showQuickFeedbackTool={showQuickFeedbackTool}
+      />
+    </section>
+  );
 }
+
+FeedbackTool.propTypes = {
+  document: PropTypes.object,
+  showQuickFeedbackTool: PropTypes.bool,
+  toggleQuickFeedback: PropTypes.func
+};
 
 export default FeedbackTool;
