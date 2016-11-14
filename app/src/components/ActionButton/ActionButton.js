f02bd65549a45f9f88d3d04f4ad287ee19226ef9
Raif Harik <reharik@gmail.com>
2016-11-14 11:51:23 -0600

    working on modal

--- app/src/components/ActionButton/ActionButton.js
+++ app/src/components/ActionButton/ActionButton.js
@@ -6,7 +6,7 @@ const ActionButton = (props) => {
   return (
     <div className={actionButton.action_button_container + ' ' + props.css}>
       <div className={actionButton.action_button_wrapper}>
-        <button data-id={props.dataId} className={actionButton.action_button}>{props.content}</button>
+        <button data-id={props.dataId} className={actionButton.action_button} onClick={props.onClick}>{props.content}</button>
       </div>
     </div>
   );
--- app/src/components/ActivitySummary/ActivityDetails/ActivityDetails.js
+++ app/src/components/ActivitySummary/ActivityDetails/ActivityDetails.js
@@ -1,36 +1,60 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 
 import activityDetails from './activityDetails.css';
+import MLModal from './../../MLModal/MLModal';
 
-// XXX need id's for testability/accessibility
+class ActivityDetails extends Component {
+  constructor() {
+    super();
+    this.state = {isOpen:false};
+    this.onClick = this.onClick.bind(this);
+    this.onClose = this.onClose.bind(this);
+  }
 
-const ActivityDetails = ({activity}) => (
-  <div className={ activityDetails.summary }>
-    <h1 data-id="activity-title" className={ activityDetails.title }>
-      {activity.title}
-    </h1>
-    <div data-id="activity-desc" className={activityDetails.typeAndCourse}>
-      {activity.desc}
-    </div>
-    <div data-id="activity-rhetoric-rubric" className={activityDetails.rhetoricAndRubric}>
+  onClick() {
+    this.setState({isOpen:true})
+  }
+
+  onClose() {
+    this.setState({isOpen:false})
+  }
+
+  render() {
+    const {activity} = this.props;
+    return (
+      <div className={ activityDetails.summary }>
+        <button onClick={this.onClick} > click me </button>
+        <h1 data-id="activity-title" className={ activityDetails.title }>
+          {activity.title}
+        </h1>
+        <div data-id="activity-desc" className={activityDetails.typeAndCourse}>
+          {activity.desc}
+        </div>
+        <div data-id="activity-rhetoric-rubric" className={activityDetails.rhetoricAndRubric}>
       <span className={activityDetails.label}>
       Rhetoric Genre:
       </span>
-      <span data-id="activity-rhetorical-genre">{activity.rhetoricalGenre}</span>
-      <br />
+          <span data-id="activity-rhetorical-genre">{activity.rhetoricalGenre}</span>
+          <br />
       <span className={activityDetails.label}>
     Rubric:
       </span>
-      <a data-id="activity-rubric" href="#">{activity.rubric}</a>
-    </div>
-    <div className={ activityDetails['prompt-summary'] }>
-      <div className={activityDetails.label}>
-        Prompt:
+          <a data-id="activity-rubric" href="#">{activity.rubric}</a>
+        </div>
+        <div className={ activityDetails['prompt-summary'] }>
+          <div className={activityDetails.label}>
+            Prompt:
+          </div>
+          <span data-id="activity-prompt">{activity.prompt}</span>
+        </div>
+        <a data-id="edit-activity" href="#">Edit Activity Information</a>
+        <MLModal titleBar={{enable:true}} isOpen={this.state.isOpen} closeModal={this.onClose} >
+          <input type="textarea" />
+        </MLModal>
       </div>
-      <span data-id="activity-prompt">{activity.prompt}</span>
-    </div>
-    <a data-id="edit-activity" href="#">Edit Activity Information</a>
-  </div>);
+    )
+  };
+}
 
 ActivityDetails.propTypes = {
   activity: PropTypes.object.isRequired
