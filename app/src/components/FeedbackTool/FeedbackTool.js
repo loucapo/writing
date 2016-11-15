268212cee14687e000204356c3e88316707b346e
Albalenys <albalenysd@gmail.com>
2016-11-15 10:48:42 -0500

    add StudentReflection component

--- app/src/components/FeedbackTool/FeedbackTool.js
+++ app/src/components/FeedbackTool/FeedbackTool.js
@@ -1,11 +1,13 @@
 import React, {PropTypes} from 'react';
 import RichTextEditor from 'react-rte';
 import SideMenu from './SideMenu/SideMenu';
+import StudentReflection from './StudentReflection/StudentReflection';
 import feedbackTool from './feedbackTool.css';
 
 const FeedbackTool = ({document}) => {
   return (
     <section>
+      <StudentReflection />
       <div className={feedbackTool.editorContainer}>
         <RichTextEditor value={document} readOnly={true} />
       </div>
---
+++ app/src/components/FeedbackTool/studentReflection/StudentReflection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import studentReflection from './studentReflection.css';
+
+const StudentReflection = () => {
+  return (
+    <div>
+      <h3>Student Reflection</h3>
+    </div>
+  );
+};
+
+export default StudentReflection;
+
