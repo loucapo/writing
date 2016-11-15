1f0c5a9523e26e40c034b23c62c5ddf2329b2462
William Sullivan <bill.sullivan@saplinglearning.com>
2016-11-15 11:23:17 -0600

    Putting the rte in the FeedbackTool in readOnly mode

--- app/src/components/FeedbackTool/FeedbackTool.js
+++ app/src/components/FeedbackTool/FeedbackTool.js
@@ -2,7 +2,7 @@ import React from 'react';
 import RichTextEditor from 'ml-react-rte';
 
 const FeedbackTool = ({value,onChange}) => {
-  return (<RichTextEditor onChange={onChange} value={value} />)
-}
+  return (<RichTextEditor onChange={onChange} value={value} readOnly='true'/>)
+};
 
 export default FeedbackTool;
