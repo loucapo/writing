1fef6ec1a62dc32402b9a318e17d03d770d8af76
Albalenys <albalenysd@gmail.com>
2016-11-15 10:54:49 -0500

    add student reflection mock data

--- app/src/components/FeedbackTool/studentReflection/StudentReflection.js
+++ app/src/components/FeedbackTool/studentReflection/StudentReflection.js
@@ -3,8 +3,16 @@ import studentReflection from './studentReflection.css';
 
 const StudentReflection = () => {
   return (
-    <div>
-      <h3>Student Reflection</h3>
+    <div className={studentReflection.studentReflectionContainer}>
+      Student Reflection
+      <p>
+        <strong>The most challenging part of this assignment was...</strong>
+        Coming up with an "arguable" claim. In the final draft, I tried to argue against someone, so I think that took the whole essay more in the direction of argument than summary.<br/>
+        <strong>I want my reader to...</strong>
+        Pay attention to the research I included. I tried hard on this part, and I'm proud of the result.<br/>
+        <strong>Finally, I want you to know that...</strong>
+        This is the longest essay I've ever had to write!
+      </p>
     </div>
   );
 };
