8a30e3878eb73719d4ab958ce282e72e64b69ff8
Albalenys <albalenysd@gmail.com>
2016-11-17 17:42:44 -0500

    fix alignment of text to icon in feedback tool sidebar

--- app/src/components/FeedbackTool/sideMenu/sideMenu.js
+++ app/src/components/FeedbackTool/sideMenu/sideMenu.js
@@ -2,19 +2,23 @@ import React from 'react';
 import sideMenu from './sideMenu.css';
 
 const SideMenu = () => {
+  let commentIcon = "https://macmillanlearning.atlassian.net/secure/attachment/21936/comment.svg",
+      thumbsUpIcon = "https://macmillanlearning.atlassian.net/secure/attachment/22111/comment_thumbsup.svg",
+      quickFeedbackIcon = "https://macmillanlearning.atlassian.net/secure/attachment/22112/comment_quickfeedback.svg";
+
   return (
     <div className={sideMenu.sidebarContainer}>
       <div className={sideMenu.sidebar}>
         <ul>
-          <li className={sideMenu.commentIcon}>Thesis</li>
-          <li className={sideMenu.commentIcon}>Reason & Support</li>
-          <li className={sideMenu.commentIcon}>Interpretation/Analysis</li>
-          <li className={sideMenu.commentIcon}>Paragraph Development</li>
-          <li className={sideMenu.commentIcon}>Integration of Research</li>
-          <li className={sideMenu.commentIcon}>Counterarguments</li>
-          <li className={sideMenu.commentIcon}>Other</li>
-          <li className={sideMenu.thumbsUpIcon}>Good Job!</li>
-          <li className={sideMenu.quickFeedbackIcon}>Quick Feedback Library</li>
+          <li><img src={commentIcon}/>Thesis</li>
+          <li><img src={commentIcon}/>Reason & Support</li>
+          <li><img src={commentIcon}/>Interpretation/Analysis</li>
+          <li><img src={commentIcon}/>Paragraph Development</li>
+          <li><img src={commentIcon}/>Integration of Research</li>
+          <li><img src={commentIcon}/>Counterarguments</li>
+          <li><img src={commentIcon}/>Other</li>
+          <li><img src={thumbsUpIcon}/>Good Job!</li>
+          <li><img src={quickFeedbackIcon}/>Quick Feedback Library</li>
         </ul>
       </div>
     </div>
