8a880ccf46d114a3c74adb2e1bd9695d52a77101
Albalenys <albalenysd@gmail.com>
2016-11-17 17:48:19 -0500

    add data-ids to feedback tool sideMenu for QA

--- app/src/components/FeedbackTool/sideMenu/sideMenu.js
+++ app/src/components/FeedbackTool/sideMenu/sideMenu.js
@@ -7,20 +7,36 @@ const SideMenu = () => {
       quickFeedbackIcon = "https://macmillanlearning.atlassian.net/secure/attachment/22112/comment_quickfeedback.svg";
 
   return (
-    <div className={sideMenu.sidebarContainer}>
-      <div className={sideMenu.sidebar}>
-        <ul>
-          <li><img src={commentIcon}/>Thesis</li>
-          <li><img src={commentIcon}/>Reason & Support</li>
-          <li><img src={commentIcon}/>Interpretation/Analysis</li>
-          <li><img src={commentIcon}/>Paragraph Development</li>
-          <li><img src={commentIcon}/>Integration of Research</li>
-          <li><img src={commentIcon}/>Counterarguments</li>
-          <li><img src={commentIcon}/>Other</li>
-          <li><img src={thumbsUpIcon}/>Good Job!</li>
-          <li><img src={quickFeedbackIcon}/>Quick Feedback Library</li>
-        </ul>
-      </div>
+    <div data-id="sideMenu" className={sideMenu.sideMenu}>
+      <ul>
+        <li data-id="thesis">
+          <img src={commentIcon}/>Thesis
+        </li>
+        <li data-id="reason&support">
+          <img src={commentIcon}/>Reason & Support
+        </li>
+        <li data-id="interpretation">
+          <img src={commentIcon}/>Interpretation/Analysis
+        </li>
+        <li data-id="paraDev">
+          <img src={commentIcon}/>Paragraph Development
+        </li>
+        <li data-id="research">
+          <img src={commentIcon}/>Integration of Research
+        </li>
+        <li data-id="counterargs">
+          <img src={commentIcon}/>Counterarguments
+        </li>
+        <li data-id="other">
+          <img src={commentIcon}/>Other
+        </li>
+        <li data-id="goodJob">
+          <img src={thumbsUpIcon}/>Good Job!
+        </li>
+        <li data-id="feedbackLib">
+          <img src={quickFeedbackIcon}/>Quick Feedback Library
+        </li>
+      </ul>
     </div>
   );
 };
