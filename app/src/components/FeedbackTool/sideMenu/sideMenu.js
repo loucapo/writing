f0658b4125368f1026a0e5f13d08ee2311ea5722
Albalenys <albalenysd@gmail.com>
2016-11-18 11:27:10 -0500

    pull sideMenu buttons into their own components

--- app/src/components/FeedbackTool/sideMenu/sideMenu.js
+++ app/src/components/FeedbackTool/sideMenu/sideMenu.js
@@ -1,41 +1,28 @@
 import React from 'react';
+import ThesisButton from './Buttons/Thesis';
+import ReasonSupportButton from './Buttons/ReasonSupport';
+import InterpretationButton from './Buttons/Interpretation';
+import ParagraphDevButton from './Buttons/ParagraphDev';
+import ResearchButton from './Buttons/Research';
+import CounterArgsButton from './Buttons/CounterArgs';
+import OtherButton from './Buttons/Other';
+import GoodJobButton from './Buttons/GoodJob';
+import FeedbackLibButton from './Buttons/FeedbackLib';
 import sideMenu from './sideMenu.css';
 
 const SideMenu = () => {
-  let commentIcon = "https://macmillanlearning.atlassian.net/secure/attachment/21936/comment.svg",
-      thumbsUpIcon = "https://macmillanlearning.atlassian.net/secure/attachment/22111/comment_thumbsup.svg",
-      quickFeedbackIcon = "https://macmillanlearning.atlassian.net/secure/attachment/22112/comment_quickfeedback.svg";
-
   return (
     <div data-id="sideMenu" className={sideMenu.sideMenu}>
       <ul>
-        <li data-id="thesis">
-          <img src={commentIcon}/>Thesis
-        </li>
-        <li data-id="reason&support">
-          <img src={commentIcon}/>Reason & Support
-        </li>
-        <li data-id="interpretation">
-          <img src={commentIcon}/>Interpretation/Analysis
-        </li>
-        <li data-id="paraDev">
-          <img src={commentIcon}/>Paragraph Development
-        </li>
-        <li data-id="research">
-          <img src={commentIcon}/>Integration of Research
-        </li>
-        <li data-id="counterargs">
-          <img src={commentIcon}/>Counterarguments
-        </li>
-        <li data-id="other">
-          <img src={commentIcon}/>Other
-        </li>
-        <li data-id="goodJob">
-          <img src={thumbsUpIcon}/>Good Job!
-        </li>
-        <li data-id="feedbackLib">
-          <img src={quickFeedbackIcon}/>Quick Feedback Library
-        </li>
+        <ThesisButton/>
+        <ReasonSupportButton/>
+        <InterpretationButton/>
+        <ParagraphDevButton/>
+        <ResearchButton/>
+        <CounterArgsButton/>
+        <OtherButton/>
+        <GoodJobButton/>
+        <FeedbackLibButton/>
       </ul>
     </div>
   );
