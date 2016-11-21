fcf8e0434a611deb18f95d3f561e95a0dee23b56
Gabriel Wilkins <gabriel.wilkins@saplinglearning.com>
2016-11-21 14:47:06 -0800

    Fixed up tests to reflect current RC & marvin
    removed boilerplate
    standardized formatting
    deleted unused tests
    uncommented and +@pending appropriate tests

--- tests/pages/instructor-assignment-summary-page.js
+++ tests/pages/instructor-assignment-summary-page.js
@@ -2,38 +2,38 @@ var Page = require('marvin-js').Page;
 
 module.exports = new Page({
 
-// assignment header
-	url: { value: '/activity/23630184-5955-4dbe-9908-ab065f1bcad2' },
-	title: { get: function () { return this.element("[data-id='activity-title']"); } },
-	// course still needs to be separated from the class name in db
-	//course: { get: function () { return this.element("[data-id='activity-course']"); } },
-	type: { get: function () { return this.element("[data-id='activity-desc']"); } },
+  // assignment header
+  url: { value: '/activity/23630184-5955-4dbe-9908-ab065f1bcad2' },
+  title: { get: function () { return this.element("[data-id='activity-title']"); } },
+  // course still needs to be separated from the class name in db
+  //course: { get: function () { return this.element("[data-id='activity-course']"); } },
+  type: { get: function () { return this.element("[data-id='activity-desc']"); } },
 
-// assignment details
-	rhetoric_genre: { get: function() { return this.element("[data-id='activity-rhetoric-rubric']"); } },
-	rubric: { get: function () { return this.element("[data-id='activity-rubric']"); } },
-	prompt: { get: function () { return this.element("[data-id='activity-prompt']"); } },
-	prompt_edit: { get: function () { return this.element("[data-id='edit-assignment']"); } },
+  // assignment details
+  rhetoric_genre: { get: function() { return this.element("[data-id='activity-rhetoric-rubric']"); } },
+  rubric: { get: function () { return this.element("[data-id='activity-rubric']"); } },
+  prompt: { get: function () { return this.element("[data-id='activity-prompt']"); } },
+  //QQQ was assignment intended to be renamed to activity?
+  prompt_edit: { get: function () { return this.element("[data-id='edit-activity']"); } },
 
-// sub menu
-	sub_menu: { get: function () { return this.element("[data-id='assignment-menu']"); } },
-	student_submissions: { get: function () { return this.element("[data-id='student-submissions']"); } },
-	
-
-// draft sequence
-	draft_names: { get: function () { return this.elements("[data-id='draft-item-draft-name']"); } },
+  // sub menu
+  sub_menu: { get: function () { return this.element("[data-id='assignment-menu']"); } },
+  student_submissions: { get: function () { return this.element("[data-id='student-submissions']"); } },
 
 
-// draft details
-	drafts_review_type: { get: function() { return this.elements("[data-id='draft-item-type']"); } },
-	drafts_due_dates: { get: function() { return this.elements("[data-id='draft-item-due-date']"); } },
+  // draft sequence
+  draft_names: { get: function () { return this.elements("[data-id='draft-item-draft-name']"); } },
 
 
-// draft learning objectives
-	draft_learning_objectives: { get: function() { return this.elements("[data-id='draft-item-learning-objectives']"); } },
+  // draft details
+  drafts_review_type: { get: function() { return this.elements("[data-id='draft-item-type']"); } },
+  drafts_due_dates: { get: function() { return this.elements("[data-id='draft-item-due-date']"); } },
 
-// draft grading policies
-	draft_grading_policies: { get: function () { return this.elements("[data-id='grading-policy']"); } },
+
+  // draft learning objectives
+  draft_learning_objectives: { get: function() { return this.elements("[data-id='draft-item-learning-objectives']"); } },
+
+  // draft grading policies
+  draft_grading_policies: { get: function () { return this.elements("[data-id='grading-policy']"); } },
 
 });
-
--- tests/steps/instructor-assignment-summary-page.js
+++ tests/steps/instructor-assignment-summary-page.js
@@ -1,109 +1,114 @@
 var page = require('../pages/instructor-assignment-summary-page.js');
-var marvin = require('marvin-js');
-var driver = marvin.session.getDriver();
-var until = require('selenium-webdriver').until;
-var should = require('chai').should;
-var expect = require('chai').expect;
-var assert = require('chai').assert;
 
 exports.define = function(steps) {
   steps.given("I visit the activity page", function() {
     page.visit();
   });
 
-	steps.then('I should see the Assignment Header elements', function() {
-		expect(page.title).to.exist;
-		expect(page.type).to.exist;
-		//expect(assignmentSummaryPage.course).to.exist;
-	});
+  steps.when("I visit the activity page", function() {
+    page.visit();
+  });
 
-	steps.then('I should see the Assignment Details elements', function() {
-		expect(page.rhetoric_genre).to.exist;
-		expect(page.rubric).to.exist;
-		expect(page.prompt).to.exist;
-		expect(page.prompt_edit).to.exist;
-	});
-  
-  
-	steps.then('the drafts should have sequence numbers and the last one is labeled "Final Draft"', function() {
-		return new Promise(function(resolve, reject) {
-			return page.draft_names.then(function(draft_names) {
-				var final_draft_name = draft_names.pop();
-				draft_names.forEach(function(name, index) {
-					name.getText().then(function(text) {
-						var expected_name = 'Draft ' + (index + 1);
-						expect(text).to.equal(expected_name);
-					});
-				});
-				final_draft_name.getText().then(function(text) {
-					var expected_name = 'Final Draft';
-					expect(text).to.equal(expected_name);
-				});
-			});
-		});
-	});
+  steps.then('I should see the Assignment Header elements', function() {
+    expect(page.title).to.exist;
+    expect(page.type).to.exist;
+    //expect(assignmentSummaryPage.course).to.exist;
+  });
 
+  steps.then('I should see the Assignment Details elements', function() {
+    expect(page.rhetoric_genre).to.exist;
+    expect(page.rubric).to.exist;
+    expect(page.prompt).to.exist;
+    expect(page.prompt_edit).to.exist;
+  });
 
-	steps.then('I can see the review types', function() {
-		return new Promise(function(resolve, reject) {
-			return page.drafts_review_type.then(function(review_types) {
-				review_types.forEach(function(type) {
-					type.getText().then(function(text) {
-						var peer = 'Peer Review';
-						var inst = 'Instructor Review';
-						assert((text == peer || text == inst), 'draft review type is not peer or instructor review');
-					});
-				});
-			});
-		});
+  steps.then('the drafts should have sequence numbers and the last one is labeled "Final Draft"', function() {
+    return new Promise(function(resolve, reject) {
+      return page.draft_names.then(function(draft_names) {
+	var final_draft_name = draft_names.pop();
+	draft_names.forEach(function(name, index) {
+	  name.getText()
+            .then(function(text) {
+	      var expected_name = 'Draft ' + (index + 1);
+	      expect(text).to.equal(expected_name);
+	    });
 	});
-	steps.then('I can also see the due dates', function() {
-		return new Promise(function(resolve, reject) {
-			return page.drafts_due_dates.then(function(due_dates) {
-				due_dates.forEach(function(date) {
-					date.getText().then(function(text) {
-						expect(text).to.be.not.empty;
-					});
-				});
-			});
-		});
-	});
+	final_draft_name.getText()
+          .then(function(text) {
+	    var expected_name = 'Final Draft';
+	    expect(text).to.equal(expected_name);
+	  });
+      });
+    });
+  });
 
-
-	steps.then('I should see some draft learning objectives', function() {
-		return new Promise(function(resolve, reject) {
-			return page.draft_learning_objectives.then(function(objectives) {
-				expect(objectives.length).to.be.above(0);
-			});
-		});
+  steps.then('I can see the review types', function() {
+    return new Promise(function(resolve, reject) {
+      return page.drafts_review_type.then(function(review_types) {
+	review_types.forEach(function(type) {
+	  type.getText()
+            .then(function(text) {
+	      var peer = 'Peer Review';
+	      var inst = 'Instructor Review';
+	      assert((text == peer || text == inst), 'draft review type is not peer or instructor review');
+	    });
 	});
+      });
+    });
+  });
 
-	steps.then('I know how each draft will be graded', function() {
-		return new Promise(function(resolve, reject) {
-			return page.draft_grade_policies.then(function(draft_grade_policies) {
-				var final_draft_grade_policy = draft_grade_policies.pop();
-				var inc_comp = 'Incomplete/Complete';
-				var letter_grade = 'Letter Grade';
-				var percentage = '%';
-				draft_grade_policies.forEach(function(policy, index) {
-					policy.getText().then(function(text) {
-						expect(text).to.equal(inc_comp);
-					});
-				});
-				final_draft_grade_policy.getText().then(function(text) {
-					assert((text == inc_comp || text == letter_grade || text == percentage), 'Final Draft grading policy incompatible');
-				});
-			});
-		});
-	});
+  steps.then('I can also see the due dates', function() {
+    return new Promise(function(resolve, reject) {
+      return page.drafts_due_dates
+        .then(function(due_dates) {
+	  due_dates.forEach(function(date) {
+	    date.getText()
+              .then(function(text) {
+	        expect(text).to.be.not.empty;
+	      });
+	  });
+        });
+    });
+  });
 
-	steps.then('I can see the student submission section', function() {
-		page.student_submissions.click();
-	});
+  steps.then('I should see some draft learning objectives', function() {
+    return new Promise(function(resolve, reject) {
+      return page.draft_learning_objectives
+        .then(function(objectives) {
+	  expect(objectives.length).to.be.above(0);
+        });
+    });
+  });
 
-	steps.then("I see the activity summary page", function() {
-		driver.wait(until.urlContains(page.url), 5000, 'target url does not contain ' + page.url);
-	});
+  steps.then('I know how each draft will be graded', function() {
+    return new Promise(function(resolve, reject) {
+      return page.draft_grade_policies
+        .then(function(draft_grade_policies) {
+	  var final_draft_grade_policy = draft_grade_policies.pop();
+	  var inc_comp = 'Incomplete/Complete';
+	  var letter_grade = 'Letter Grade';
+	  var percentage = '%';
+	  draft_grade_policies.forEach(function(policy, index) {
+	    policy.getText()
+              .then(function(text) {
+	        expect(text).to.equal(inc_comp);
+	      });
+	  });
+	  final_draft_grade_policy.getText()
+            .then(function(text) {
+	      assert((text == inc_comp || text == letter_grade || text == percentage), 'Final Draft grading policy incompatible');
+	    });
+        });
+    });
+  });
+
+  steps.then('I can see the student submission section', function() {
+    page.student_submissions.click();
+  });
+
+  steps.then("I see the activity summary page", function() {
+    driver.wait(until.urlContains(page.url), 5000, 'target url does not contain ' + page.url);
+  });
 
   steps.when("I see my course data has reset", function () {
     //steps to be added once page has something changeable
@@ -112,4 +117,4 @@ exports.define = function(steps) {
   steps.given("I visit the home page", function () {
     driver.get(marvin.config.baseUrl);
   });
-}
+};
