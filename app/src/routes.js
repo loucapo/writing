7d7d48c9aa5d7f8ac232a9a07f087f7bf8ed1cb2
Brook Davis <brook.davis@saplinglearning.com>
2016-11-21 14:13:45 -0600

    Remove EditorWrapper from routes.  Remove /editor route.

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Route, IndexRedirect} from 'react-router';
 import ActivityContainer from './containers/ActivityContainer';
-import EditorWrapper from './components/EditorWrapper/EditorWrapper';
 import DashBoard from './components/DashBoard/DashBoard';
 import CreateActivityContainer from './containers/CreateActivityContainer';
 import AppContainer from './containers/AppContainer';
@@ -12,7 +11,6 @@ let redirectActivity = '/activity/23630184-5955-4dbe-9908-ab065f1bcad2';
 const routes = (
   <Route path="/" component={AppContainer}>
     <IndexRedirect to={redirectActivity} />
-    <Route path="/editor" component={EditorWrapper} />
     <Route path="/dashboard" component={DashBoard} />
     <Route path="/create" component={CreateActivityContainer} />
     <Route path="/activity/:id" component={ActivityContainer} />
