bba2b9668934c1b57af257164dda2f7b9e06a1e0
Albalenys <albalenysd@gmail.com>
2016-11-22 16:16:06 -0500

    fix textarea placeholder error and npm lint syntax error

--- app/src/components/FeedbackTool/EndComment/EndComment.js
+++ app/src/components/FeedbackTool/EndComment/EndComment.js
@@ -5,7 +5,7 @@ const EndComment = () => {
   return (
     <div data-id="endComment" className={endComment.endCommentContainer}>
       <h4>End Comment (Optional)</h4>
-      <textarea>Write any concluding remarks you want to tell the student...</textarea>
+      <textarea placeholder="Write any concluding remarks you want to tell the student..."/>
     </div>
   );
 };
