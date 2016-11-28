8639e45674ffd1baced7984a8088859f4d0f259c
Lou Capozzoli <lou@latenitelabs.com>
2016-11-28 12:48:56 -0500

    WRITE-388: removed some placeholder logic that would, maybe, be used for the future.

--- app/src/containers/RubricContainer.js
+++ app/src/containers/RubricContainer.js
@@ -12,12 +12,6 @@ class RubricContainer extends Component {
   }
 
   render() {
-    if (this.props.isFetching) {
-      return (<p style={{ 'padding-top': '100px' }}> Loading... </p>);
-    }
-    if (this.props.errorMessage) {
-      return (<p style={{ 'padding-top': '100px' }}>ERROR! -> {this.props.errorMessage}</p>);
-    }
     return (<Rubric rubric={this.props.rubric} showRubric={this.props.showRubric} />);
   }
 }
