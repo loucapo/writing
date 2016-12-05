5ea0a1592595ac1764a72f2a7bbe8aec0dcb7c87
Raif Harik <reharik@gmail.com>
2016-12-05 17:44:03 -0600

    WRITE-57 removed some default data for flags that didn't need to be there anymore

--- app/src/modules/feedbackToolContentModule.js
+++ app/src/modules/feedbackToolContentModule.js
@@ -1,36 +1,7 @@
 export const CREATE_FEEDBACK_TOOL_CONTENT_ITEM = 'wk_frontend/feedbackTool/CREATE_FEEDBACK_TOOL_CONTENT_ITEM';
 
-const feedbackToolContentItem = [{
-  type: 'integrationOfResearch',
-  instructorContent: {
-    comment: `You do a nice job presenting these two sides; however, you're not staking a claim in this argument.
-  Your thesis is buried and unclear.
-  I would begin here with your revisions to clarify your thesis statement.`
-  },
-  publisherContent: {
-    resources: [
-      {
-        title: 'What is a Thesis',
-        url: 'http://www.google.com'
-      },
-      {
-        title: 'Examples of a good Thesis',
-        url: 'http://www.facebook.com'
-      },
-      {
-        title: 'Where should I put my Thesis',
-        url: 'http://www.yahoo.com'
-      }
-    ]
-  },
-  position: {
-    top: '400',
-    left: '0'
-  }
-}];
-
 // Reducer
-export default (state = feedbackToolContentItem, action) => {
+export default (state = [], action) => {
   switch (action.type) {
     case CREATE_FEEDBACK_TOOL_CONTENT_ITEM: {
 
