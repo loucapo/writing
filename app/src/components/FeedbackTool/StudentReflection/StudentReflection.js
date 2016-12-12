3ae3efa21bedfff5bf21f67f8ed24c7f165ffdfc
Lou Capozzoli <lou@latenitelabs.com>
2016-12-12 12:56:12 -0500

    WRITE-581: styling for the student reflections panel.

--- app/src/components/FeedbackTool/StudentReflection/StudentReflection.js
+++ app/src/components/FeedbackTool/StudentReflection/StudentReflection.js
@@ -5,20 +5,34 @@ import coreCss from './../../../styles/core.css';
 const StudentReflection = () => {
   /*eslint-disable */
   return (
-    <div data-id="studentReflections" className={coreCss.box + ' ' + studentReflectionCss.studentReflectionContainer}>
-      <h4>Student Reflection</h4>
-      <p>
-        <strong>The most challenging part of this assignment was...</strong>
-        Coming up with an "arguable" claim. In the final draft, I tried to argue against someone,
-        so I think that took the whole essay more in the direction of argument than summary.
-      </p>
-      <p>
-        <strong>I want my reader to...</strong> Pay attention to the research I included.
-        I tried hard on this part, and I'm proud of the result.
-      </p>
-      <p>
-        <strong>Finally, I want you to know that...</strong> This is the longest essay I've ever had to write!
-      </p>
+    <div className={ coreCss.panel }>
+    {/*<div data-id="studentReflections" className={coreCss.box + ' ' + studentReflectionCss.studentReflectionContainer}>*/}
+      <div data-id="studentReflections">
+        <h1>
+          Student Reflection
+        </h1>
+      </div>
+      <section className={ studentReflectionCss.section }>
+        <h4>The most challenging part of this assignment was...</h4>
+        <div>
+          Coming up with an "arguable" claim. In the final draft, I tried to argue against someone,
+          so I think that took the whole essay more in the direction of argument than summary.
+          <p></p>
+        </div>
+
+        <h4>I want my reader to...</h4>
+        <div>
+          Pay attention to the research I included.
+          <p>
+            I tried hard on this part, and I'm proud of the result.
+          </p>
+        </div>
+
+        <h4>Finally, I want you to know that...</h4>
+        <div>
+            This is the longest essay I've ever had to write!
+        </div>
+      </section>
     </div>
   );
  /*eslint-enable */
