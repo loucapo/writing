bc0e5942243c2d9d8d37219ba37cdd404e01ddb6
Albalenys <albalenysd@gmail.com>
2016-12-21 17:00:47 -0500

    WRITE-604: Dynamically change color of comment flag border

--- app/src/components/FeedbackTool/FeedbackToolContentFlags/FeedbackToolContentFlag/FeedbackToolContentFlag.js
+++ app/src/components/FeedbackTool/FeedbackToolContentFlags/FeedbackToolContentFlag/FeedbackToolContentFlag.js
@@ -2,18 +2,31 @@ import React, { PropTypes } from 'react';
 import MLIcon from 'ml-react-cdl-icons';
 import FlagDetails from './FlagDetails/FlagDetails';
 import feedbackToolContentMap from './../../feedbackToolContentMap';
-import feedbackToolContentFlag from './feedbackToolContentFlag.css';
+import flagStyles from './feedbackToolContentFlag.css';
 
 const FeedbackToolContentFlag = ({item, expanded, topFlag, onClick}) => {
   const triangleStyles = {
     top: `${item.position.top}px`,
-    zIndex: topFlag ? 1001 : 1
+    zIndex: topFlag ? 1001 : 1,
+    border: `1px solid ${item.color}`
   };
 
+  let borderPointerColor;
+  switch(item.color) {
+    case '#00758E':
+      borderPointerColor = flagStyles.blueTriangleBorder;
+      break;
+    case '#dd5714':
+      borderPointerColor = flagStyles.orangeTriangleBorder;
+      break;
+    case '#3b822e':
+      borderPointerColor = flagStyles.greenTriangleBorder;
+  }
+
   return (
-    <div className={feedbackToolContentFlag.flagContainer} onClick={() => onClick(!expanded, item.id)}>
-      <div className={feedbackToolContentFlag.triangleBorder} style={triangleStyles}>
-        <div className={feedbackToolContentFlag.heading}>
+    <div className={flagStyles.flagContainer} onClick={() => onClick(!expanded, item.id)}>
+      <div className={`${flagStyles.triangleBorder} ${borderPointerColor}`} style={triangleStyles}>
+        <div className={flagStyles.heading}>
           <MLIcon
             iconTitle={feedbackToolContentMap[item.contentType].title}
             iconType="comment"
@@ -23,7 +36,7 @@ const FeedbackToolContentFlag = ({item, expanded, topFlag, onClick}) => {
             viewBox="0 0 24 24"
           />
           <strong>{feedbackToolContentMap[item.contentType].title}</strong>
-          <span className={feedbackToolContentFlag.icon} />
+          <span className={flagStyles.icon} />
         </div>
         {expanded ? <FlagDetails item={item} /> : null}
       </div>
