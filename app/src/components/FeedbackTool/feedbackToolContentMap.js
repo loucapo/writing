09205867a4703c2c41ead72502a50012dde3575f
Lou Capozzoli <lou@latenitelabs.com>
2016-12-22 17:40:40 -0500

    WRITE-612: replace colors

--- app/src/components/FeedbackTool/feedbackToolContentMap.js
+++ app/src/components/FeedbackTool/feedbackToolContentMap.js
@@ -4,7 +4,6 @@ import { standardForm } from './SideMenu/Buttons/modalForm';
 const feedbackToolContentMap = {
   thesis: {
     title: 'Thesis',
-    color: 'blue',
     resourceLinks: [4, 5, 6],
     model: standardModel,
     form: standardForm,
@@ -13,42 +12,42 @@ const feedbackToolContentMap = {
   },
   reasonSupport: {
     title: 'Reason and Support',
-    color: 'blue',
+    color: '#00758E',
     resourceLinks: [7, 8, 9],
     model: standardModel,
     form: standardForm
   },
   interpretation: {
     title: 'Interpretation',
-    color: 'blue',
+    color: '#00758E',
     resourceLinks: [4, 5, 6],
     model: standardModel,
     form: standardForm
   },
   paragraphDev: {
     title: 'Paragraph Development',
-    color: 'blue',
+    color: '#00758E',
     resourceLinks: [4, 5, 6],
     model: standardModel,
     form: standardForm
   },
   research: {
     title: 'Integration of Research',
-    color: 'blue',
+    color: '#00758E',
     resourceLinks: [4, 5, 6],
     model: standardModel,
     form: standardForm
   },
   counterarg: {
     title: 'Counterarguments',
-    color: 'blue',
+    color: '#00758E',
     resourceLinks: [4, 5, 6],
     model: standardModel,
     form: standardForm
   },
   other: {
     title: 'Other',
-    color: 'blue',
+    color: '#00758E',
     resourceLinks: [4, 5, 6],
     model: standardModel,
     form: standardForm
@@ -63,7 +62,7 @@ const feedbackToolContentMap = {
   },
   fragment: {
     title: 'Fragment',
-    resourceLinks: [4, 5, 6]
+    resourceLinks: [4, 5, 6],
     form: standardForm,
     icon: 'comment_text',
     color: '#dd5714'
