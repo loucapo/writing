021bbcc657442209674acc061d74f53eb81c0f5b
Wayne Ng <wayne.ng@saplinglearning.com>
2017-03-08 15:24:54 -0600

    more stubbing out tests

--- tests/pages/NextGen Writer Key/admin-production-tools-page.js
+++ tests/pages/NextGen Writer Key/admin-production-tools-page.js
@@ -1,17 +1,7 @@
-var page = require('../../pages/NextGen Writer Key/sls-user-credentials.js');
+var Page = require('marvin-js').Page;
 
-exports.define = function(steps) {
-  steps.given('I login as "$user"', function(user) {
-    credentials.visit();
-    credentials.sls_username.sendKeys(credentials[user]);
-    credentials.sls_password.sendKeys(credentials.sls_dev_password);
-    credentials.sls_login.click();
-  });
+module.exports = new Page({
 
-  steps.when("I launch into the writing production tool", function () {
-    moodle_lti_launch_page.visit();
-    // unused?
-    // let launch_page_url = driver.getCurrentUrl();
-    moodle_lti_launch_page.test_lti_link.click();
-  });
-};
+  url: { value: 'production/tools/' },
+
+});
