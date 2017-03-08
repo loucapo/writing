a4258b213f0960dcb2ec498797b73971529740e4
Raif Harik <reharik@gmail.com>
2017-03-07 18:53:54 -0600

    had to comment out draftModule as it was blowing up

--- app/src/modules/draftModule.js
+++ app/src/modules/draftModule.js
@@ -4,16 +4,16 @@ export const SUCCESS_ACTIVITY = 'wk_frontend/activity/SUCCESS_ACTIVITY';
 
 // Reducer
 export default (state = [], action) => {
-  switch (action.type) {
-    case SUCCESS_ACTIVITY: {
-      let drafts = action.result.data.drafts;
-      if (!drafts) {
-        return state;
-      }
-      return reducerMerge(state, drafts);
-    }
-    default: {
+  // switch (action.type) {
+  //   case SUCCESS_ACTIVITY: {
+  //     let drafts = action.result.data.drafts;
+  //     if (!drafts) {
+  //       return state;
+  //     }
+  //     return reducerMerge(state, drafts);
+  //   }
+  //   default: {
       return state;
-    }
-  }
+    // }
+  // }
 };
