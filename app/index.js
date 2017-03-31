adf61efc8144774d65794ee3f51b8a93c23eab06
Raif Harik <raif.harik@saplinglearning.com>
2017-03-31 20:30:07 +0000

    Merged in remove_babel_dependency (pull request #28)
    removed babel
    
    Approved-by: Abby <abby.delossantos.contractor@macmillan.com>
    Approved-by: Brook Davis <brook.davis@saplinglearning.com>

--- app/index.js
+++ app/index.js
@@ -1,12 +1,6 @@
 process.env['ALLOW_CONFIG_MUTATIONS'] = true; // eslint-disable-line dot-notation
 const registry = require('./registry');
-const env = process.env.NODE_ENV || 'development';
 
-require('babel-polyfill');
-if (env === 'development') {
-  // for development use babel/register for faster runtime compilation
-  require('babel-register');
-}
 module.exports = (function() {
   const container = registry();
   const api = container.getInstanceOf('server');
