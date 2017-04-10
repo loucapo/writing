9cfa65a0b4a92d7b15a726820c67d77e6c189c79
Gabriel Wilkins <gabriel.wilkins@saplinglearning.com>
2017-04-10 08:45:23 -0700

    The shortest of supertest step demos

---
+++ tests/steps/NextGen Writer Key/supertest.js
@@ -0,0 +1,16 @@
+var request = require('supertest');
+
+exports.define = function(steps) {
+
+    steps.given("I send an empty POST body to '$url' and recieve status '$stat'", function(url, stat, done) {
+        request(marvin.config.baseUrl)
+            .post(url)
+            .expect(parseInt(stat), done);
+    });
+
+    steps.given("I send an empty POST body to '$url' and recieve status '$stat' promise-style", function(url, stat) {
+        return request(marvin.config.baseUrl)
+            .post(url)
+            .expect(parseInt(stat));
+    });
+};
