fe6f0d16fe4aa1617d8e76b393ca5d0b46723e77
Gabriel Wilkins <gabriel.wilkins@saplinglearning.com>
2017-04-10 18:38:49 -0700

    initial refactor nearly done

--- app/src/repository.js
+++ app/src/repository.js
@@ -6,36 +6,35 @@ const pgformat = require('pg-format');
 var debug = true;
 
 function makeAdapter (connection, debug) {
-  return {
-    query: function (query) {
-      if (debug) {
-        console.log('\n\nPuresql MSSQL adapter: ');
-        console.log(query)
-      }
-      return new connection.query(query)
-    },
-    escape: function (parameter) {
-      if (+parameter === parameter) {
-        return parameter
-      } else {
-        return pgformat.literal(parameter)
-      }
+    return {
+        query: function (query) {
+            if (debug) {
+                console.log('\n\nPuresql MSSQL adapter: ');
+                console.log(query)
+            }
+            return new connection.query(query)
+        },
+        escape: function (parameter) {
+            if (+parameter === parameter) {
+                return parameter
+            } else {
+                return pgformat.literal(parameter)
+            }
+        }
     }
-  }
 }
 
 module.exports =  function (queriesFile, query, event) {
-  const pg = new pgasync.default(config.postgres.config);
-  var adapter = makeAdapter(pg);
-  var queries = puresql.loadQueries(queriesFile);
+    const pg = new pgasync.default(config.postgres.config);
+    var adapter = makeAdapter(pg);
+    var queries = puresql.loadQueries(queriesFile);
 
-  queries[query](event, adapter)
+    queries[query](event, adapter)
         .then((rows)=> {
-            console.log("HOOO AHHH");
-      return rows
-    })
-    .catch((error) => {
-      console.log(error);
-      throw error;
-    });
+            return rows
+        })
+        .catch((error) => {
+            console.log(error);
+            throw error;
+        });
 };
