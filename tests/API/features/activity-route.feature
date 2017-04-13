adc5318cc9758d74e2afe9369cc41c828de142dc
Wayne Ng <wayne.ng@saplinglearning.com>
2017-04-13 09:55:08 -0500

    pass a cookie to different step

--- tests/API/features/activity-route.feature
+++ tests/API/features/activity-route.feature
@@ -3,4 +3,4 @@ Feature: Activity API Routes
 @only
   Scenario: get homepage 2
     Given I get a cookie '200'
-    Given I GET '/resource/d3e3c2d5-cf43-4f63-924f-3ec7a125a334' and receive status '200'"
+    Given I GET '/activity/d3e3c2d5-cf43-4f63-924f-3ec7a125a334' and receive status '200'"
--- tests/API/steps/supertest.js
+++ tests/API/steps/supertest.js
@@ -5,10 +5,6 @@ exports.define = function(steps) {
   steps.given("I get a cookie '$stat'", function(stat, done) {
     this.ctx.cookies = [];
     cookies = this.ctx.cookies;
-    // go to baseurl/instructor
-    // read the 'Set-Cookie' headers from the response
-    // save those cookies in cookies
-
     request('http://wk-qa-2.mldev.cloud')
       .get('/instructor')
       .expect(stat)
@@ -20,14 +16,9 @@ exports.define = function(steps) {
 
   steps.given("I GET '$url' and receive status '$stat'", function(url, stat, done) {
     req.cookies = this.ctx.cookies;
-    console.log(cookies);
     request(marvin.config.baseUrl)
-    // set the cookies to send in this get from this.ctx
       .get(url)
-      .end(function (err, res) {
-      req.cookies = this.ctx.cookies;
-      console.log(cookies);
-    })
+      .set('Cookie',[cookies])
       .expect(parseInt(stat), done);
   });
 
