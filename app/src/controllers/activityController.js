2970588d38d48c9c976bf1ed759aa15ef68e1605
Raif Harik <reharik@gmail.com>
2017-04-26 14:19:28 -0500

    fixing a bug that is in develop

--- app/src/controllers/activityController.js
+++ app/src/controllers/activityController.js
@@ -34,7 +34,7 @@ module.exports = function(domain, repository, sqlLibrary, domainBuilders, logger
           createdById: ctx.state.user.user_data.id
         });
 
-        await repository.query(sqlLibrary.draft, 'addDraftToActivity', draftEvent.event);
+        await repository.query(sqlLibrary.draft, 'addDraftToActivity', draftEvent);
       }
       logger.debug(`Call to createActivity successful with following payload: ${JSON.stringify(command)}`);
 
