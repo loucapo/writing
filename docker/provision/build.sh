dd005f1c9fc2f8f210925fac560239397891248c
Raif Harik <reharik@gmail.com>
2017-05-30 13:07:57 -0500

    trying to break build so I can test handling

--- docker/provision/build.sh
+++ docker/provision/build.sh
@@ -51,10 +51,10 @@ cat artifacts/.env
 echo "Building docker images and deployment artifacts"
 
 #docker-compose -f docker/docker-compose-build.yml build |
- #xargs docker inspect -f '{{ .State.ExitCode }}' | grep -vxq echo'[^0]'
+ xargs docker inspect -f '{{ .State.ExitCode }}' | grep -vxq echo'[^0]'
 
 docker-compose -f docker/docker-compose-build.yml push |
- #xargs docker inspect -f '{{ .State.ExitCode }}' | grep -vxq echo'[^0]'
+ xargs docker inspect -f '{{ .State.ExitCode }}' | grep -vxq echo'[^0]'
 
 rm docker/.envrc.example
 
