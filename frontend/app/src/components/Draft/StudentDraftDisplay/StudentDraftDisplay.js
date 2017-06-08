8e35ffd906df081c86e4bffaeda9fc90390fffc5
Abby <abby.delossantos.contractor@macmillan.com>
2017-06-08 21:38:37 +0000

    Merged in WRITE-41.1 (pull request #66)
    WRITE-41.1
    
    * mapped 41 changes onto dev
    
    * Fix text on draft buttons
    
    * (WIP) Wire submitted drafts to read-only version
    
    * Add section for reflection questions in CompositionDisplay.js
    
    * Wire 'View Activity Summary' button
    
    * Fix css in CompositionDisplayHeader
    
    * Add readOnly route for compositions
    
    * Display cardTitle in CompositionDisplay instead of hardcoded title
    
    * List reflection questions in read-only draft
    
    * Add reflection answers to read-only draft
    
    * Add success submitted draft msg (need to fix date)
    
    * Fix lint errors
    
    * Fix compsitionDisplay.css spacing
    
    * Clean up containers
    
    * Fix merge conflict
    
    * added studentDraftId to studentInfo in draft selector
    
    * Further clean up containers
    
    * Use studentDraftId in composition display route
    
    * Fix lint errors
    
    * Write-41.1 refactor container
    
    * Write-41.1 refactor container
    
    * Fix errors
    
    Approved-by: Lou Capozzoli <lou.capozzoli.contractor@macmillan.com>
    Approved-by: Raif Harik <raif.harik@saplinglearning.com>

--- frontend/app/src/components/Draft/StudentDraftDisplay/StudentDraftDisplay.js
+++ frontend/app/src/components/Draft/StudentDraftDisplay/StudentDraftDisplay.js
@@ -10,29 +10,33 @@ const StudentDraftDisplay = ({ draft, activityId }) => {
   let finalInstruct = draft.studentInfo.disabled && draft.studentInfo.status === 'notStarted'
     ? `You will be able to view and start this draft once you've received feedback on Draft ${draft.index}`
     : '';
-
   return (
     <MLCard key={draft.draftId} type="draft" title={draft.studentInfo.title} disabled={draft.studentInfo.disabled}>
       <div className={styles.side}>
-        {draft.studentInfo.status === 'submitted' ?
-          <span data-id="submitted-date" className={styles.submittedDate}>
-            <MLIcon
-              className={styles.checkIcon}
-              title="check"
-              type="check"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-            />
-            <strong>Submitted</strong> {moment(draft.submittedDate).format('MMMM Do, YYYY')}
-          </span>
+        {draft.studentInfo.status === 'submitted'
+          ?
+            <span data-id="submitted-date" className={styles.submittedDate}>
+              <MLIcon
+                className={styles.checkIcon}
+                title="check"
+                type="check"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              />
+              <strong>Submitted</strong> {moment(draft.submittedDate).format('MMMM Do, YYYY')}
+            </span>
           : null}
         <MLButton
           id="startDraft"
           title={draft.studentInfo.buttonText}
           dataId="start-draft"
           disabled={draft.studentInfo.disabled}
-          link={draft.studentInfo ? `/activity/${activityId}/draft/${draft.draftId}` : ''}
+          link={
+            draft.studentInfo.status === 'submitted'
+              ? `/studentDraft/${draft.studentInfo.studentDraftId}/display`
+              : `/activity/${activityId}/draft/${draft.draftId}`
+          }
         />
       </div>
       <DraftDisplay
@@ -50,7 +54,7 @@ const StudentDraftDisplay = ({ draft, activityId }) => {
 StudentDraftDisplay.propTypes = {
   draft: PropTypes.object,
   activityId: PropTypes.string,
-  finalDraft: PropTypes.bool
+  studentActivityId: PropTypes.string
 };
 
 export default StudentDraftDisplay;
