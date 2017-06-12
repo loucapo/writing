18db2fb4e147bd4426f4b96bbc30bb51591848fa
Langel <jason.langel@macmillan.com>
2017-06-12 19:29:46 +0000

    Merged in WRITE-823-QA (pull request #74)
    WRITE-823 QA
    
    * started updating stubs
    
    * close to done until we get succes message
    
    * removing incorrect parenthesis
    
    * fleshed out student reflection question tests
    
    Approved-by: Wayne Ng <wayne.ng@macmillan.com>
    Approved-by: Gabriel Wilkins <gabriel.wilkins.contractor@macmillan.com>

--- test/tests/pages/NextGen Writer Key/student-assignment-draft-page.js
+++ test/tests/pages/NextGen Writer Key/student-assignment-draft-page.js
@@ -4,7 +4,7 @@ module.exports = new Page({
   url: { value: '/student' },
   start_draft_1_button: {
     get() {
-      return this.element("[data-id='start-draft']");
+      return this.element("[data-id='start-draft']:first-of-type");
     }
   },
   start_final_paper_button: {
@@ -81,5 +81,53 @@ module.exports = new Page({
     get() {
       return this.element("[data-id='rubric-arrow-right']");
     }
+  },
+  drag_submission_confirmation_modal: {
+    get() { return this.element("[class*='MLDialog__alert']"); }
+  },
+  draft_submission_cancel: {
+    get() { return this.element("button[data-id='dialog-cancel']"); }
+  },
+  draft_submission_submit: {
+    get() { return this.element("button[data-id='dialog-submit']"); }
+  },
+  draft_submission_confirmation_banner: {
+    get() { return this.element("[class*='MLDialog__alert_container']"); }
+  },
+  draft_submission_confirmation_success_icon: {
+    get() { return this.element("[class*='MLMessage__message_icon_success']"); }
+  },
+  reflection_questions: {
+    get() {
+      return this.element("[class^='ReflectionQuestionsForm__reflection']");
+    }
+  },
+  reflection_textfields: {
+    get() {
+      return this.elements("[class^='ReflectionQuestionsForm__reflection'] textarea");
+    }
+  },
+  reflection_polls: {
+    get() {
+      return this.elements("[class^='ReflectionQuestionsForm__reflection'] form");
+    }
+  },
+  reflection_polls_first_options: {
+    get() {
+      return this.elements("[class^='ReflectionQuestionsForm__reflection'] form input:first-child");
+    }
+  },
+  reflection_button_submit_enabled: {
+    get() {
+      return this.element("[data-id='submit-draft']:not([class*='MLButton__disabled'])");
+    }
+  },
+  reflection_button_submit_disabled: {
+    get() {
+      return this.element("[data-id='submit-draft'][class*='MLButton__disabled']");
+    }
+  },
+  view_final_draft_button: {
+    get() { return this.element("[data-id='View Final Paper']"); }
   }
 });
