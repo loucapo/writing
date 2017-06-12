b15acc096c122e16a4a79d518bf337646f8cc914
Bill Sullivan <bill.sullivan@macmillan.com>
2017-06-12 20:00:18 +0000

    Merged in fix-casing-for-deploy (pull request #73)
    Reverting fix casing for deploy
    
    * Fix the letter casing that caused a deploy to fail
    
    * Reverting casing change because it didn't fix anything
    
    * Reverting casing change because it didn't fix anything
    
    * Fixing typo
    
    Approved-by: Brook Davis <brook.davis@saplinglearning.com>
    Approved-by: Lou Capozzoli <lou.capozzoli.contractor@macmillan.com>

--- frontend/app/src/components/Composition/CompositionDisplayHeader/CompositionDisplayHeader.js
+++ frontend/app/src/components/Composition/CompositionDisplayHeader/CompositionDisplayHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './CompositionDisplayHeader.css';
+import styles from './compositionDisplayHeader.css';
 
 const Header = ({ homeRoute }) => {
   return (
