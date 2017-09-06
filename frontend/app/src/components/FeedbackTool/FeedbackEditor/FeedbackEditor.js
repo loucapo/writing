bac9cf38768c76b5e7266fcd0cf5eb52ca841ea6
Abby <abby.delossantos.contractor@macmillan.com>
2017-09-06 14:35:23 -0400

    Pull out sanitize-html (will be pulled into seperate ticket)

--- frontend/app/src/components/FeedbackTool/FeedbackEditor/FeedbackEditor.js
+++ frontend/app/src/components/FeedbackTool/FeedbackEditor/FeedbackEditor.js
@@ -17,23 +17,12 @@ class FeedbackEditor extends Component {
 
   componentWillReceiveProps = (nextProps) => {
     const newFeedback = !_.isEqual(this.props.lastFeedback, nextProps.lastFeedback);
-    let htmlContent = document.getElementById('feedbackEditor').innerHTML;
-    let allowedAttributes = Object.assign(
-      {
-        span: ['data-feedbackid', 'class']
-      }, 
-      sanitizeHtml.defaults.allowedAttributes
-    );
-    let cleanContent = sanitizeHtml(htmlContent, {
-      allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'span' ]),
-      allowedAttributes
-    });
 
     if(this.state.saving && newFeedback) {
       this.addHighlights(nextProps.lastFeedback.feedbackId);
       this.setState({
         showCommentModal: false,
-        content: cleanContent
+        content: document.getElementById('feedbackEditor').innerHTML
       }, () => {
         this.props.updateFeedbackPaper(this.props.studentActivityId, this.props.studentDraftId, this.state.content);
       });
