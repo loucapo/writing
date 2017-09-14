3469398256b7585ef2f97e3692856a6a66e16042
Abby <abby.delossantos.contractor@macmillan.com>
2017-09-14 19:42:41 +0000

    Merged in WRITE-983 (pull request #170)
    WRITE-983
    
    * Add level column to feedback table
    
    * Pass level to createFeedback
    
    * WRITE-1199: add tag component and apply logic to level buttons
    
    * WRITE-1199: handle nulling of comment.
    
    * Display feedback highlights
    
    * Use classes instead to display highlights
    
    * Use wrapper to display highlights
    
    * (WIP) Pass feedbackId to highlights
    
    * (WIP) Render flags in Highlight component
    
    * WRITE-1213: highlighting now saving and flag css and html completed.
    
    * WRITE-1213: Feedback IDs are now saving into the feedback HTML
    
    * WRITE-1213: Removing debugger statements
    
    * WRITE-1213: we've got highlights saving, reloading and the comment flag where it belongs!!! go team!
    
    * (WIP) Style comment flags
    
    * Finish styling feedback flag
    
    * WRITE-1199: making flag colors change based on predefined good job selection
    
    * First pass at sanitizing html
    
    * Pull out sanitize-html (will be pulled into seperate ticket)
    
    * Fix yarn errors
    
    * Remove draft-convert package
    
    * Remove sanitize-html package
    
    * WRITE-1199: add basic error handling to the comment modal.
    
    * WRITE-1199: remove a left over console log.
    
    * WRITE-1199: fix bad merge that reverted feedback-id to feedbackId
    
    * WRITE-1199: oops missed a closing bracket in the many merges i did.
    
    * WRITE-1198:
        # convert feedback flag to a component
        # add styles and content that show when its expanded
        # add expand / collapse logic
    
    * WRITE-1199: remove an old todo.
    
    * WRITE-1199: add zindex to expanded flag.
    
    * WRITE-1199: remove global css highlight style and point to local one instead.
    
    * WRITE-1199: create flags component to control which flag is expanded, allowing only 1 at a time.
    
    * WRITE-1199: few minor tweaks.
    
    * Display highlight and feedback flags
    
    * Fix positioning of feedback flags
    
    * Fix yarn errors
    
    * Fix yarn errors
    
    * Undo changes in FeedbackFlag.js
    
    * Undo previous changes
    
    * Remove unnecessary importants from feedbackDisplay.css
    
    * Remove whitespace
    
    Approved-by: Lou Capozzoli <lou.capozzoli.contractor@macmillan.com>
    Approved-by: Alex Ingraham <alex.ingraham@macmillan.com>

--- frontend/app/src/components/FeedbackTool/FeedbackDisplay/FeedbackDisplay.js
+++ frontend/app/src/components/FeedbackTool/FeedbackDisplay/FeedbackDisplay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { MLEditor, MLCard } from '../../MLComponents';
-import { FeedbackDisplayHeader } from '../index.js';
+import { MLCard } from '../../MLComponents';
+import { FeedbackDisplayHeader, FeedbackFlags } from '../index.js';
 import { RubricDisplayContainer } from '../../../containers';
 import styles from './feedbackDisplay.css';
 
@@ -16,7 +16,8 @@ const FeedbackDisplay = ({
   rubricId,
   lastDraft,
   linkableDrafts,
-  noRubricScores
+  noRubricScores,
+  feedback
 }) => (
   <div className={styles.page}>
     <FeedbackDisplayHeader
@@ -37,16 +38,20 @@ const FeedbackDisplay = ({
           : null
         }
         { (lastDraft && !studentDraft.finalGrade) ?
-          <div>
-            <span className={styles.finalGradeLabel}>Instructor did not select a final score</span>
-          </div>
+          <span className={styles.finalGradeLabel}>Instructor did not select a final score</span>
           : null
         }
         {studentDraft.endComment ? <span>{studentDraft.endComment}</span> : <span>No end comment added.</span>}
       </MLCard>
 
       <MLCard type="draft" title={draftTitle}>
-        <MLEditor content={studentDraft.paper} editable={false} toolbarHidden={true} />
+        <div className={styles.feedbackPaperWrapper}>
+          <div
+            className={styles.feedbackPaper}
+            dangerouslySetInnerHTML={{ __html: studentDraft.feedbackPaper }}
+          />
+          <FeedbackFlags feedback={feedback} />
+        </div>
       </MLCard>
 
       <MLCard type="reflection" title="Reflection">
@@ -90,7 +95,8 @@ FeedbackDisplay.propTypes = {
   backLink: PropTypes.string,
   backText: PropTypes.string,
   linkableDrafts: PropTypes.array,
-  noRubricScores: PropTypes.bool
+  noRubricScores: PropTypes.bool,
+  feedback: PropTypes.array
 };
 
 export default FeedbackDisplay;
