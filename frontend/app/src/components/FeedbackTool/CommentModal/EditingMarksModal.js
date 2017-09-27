b19e866357246334e299b65b0b346834ba14c4a2
Lou Capozzoli <lou.capozzoli.contractor@macmillan.com>
2017-09-27 19:56:38 +0000

    Merged in WRITE-1440 (pull request #201)
    WRITE-1440
    
    * WRITE-1442: initial commit contains:
    
        # removing left over add comment modal
        # creates opencommentmodal with contents of former commentmodal
        # re-uses commentmodal as a container for the three modals
        # updates feedback editor to use new commentmodal component
    
    * WRITE-1442: a little clean up.
    
    * WRITE-1442: refactor up some components in prep of the new modals.
    
    * WRITE-1442: Convert to dynamic component names and a modal for each
    
    * WRITE-1442: update draft goals modal to work with refactors.
    
    * WRITE-1442: fix a sizing issue
    
    * WRITE-1442: restore background click and fix some more spacing stuff.
    
    * Add content in EditingMarksModal
    
    * Make Editing Mark Menu functional
    
    * Clean up EditingMarksModal.js
    
    * add handleDraftGoalChange() to DraftGoalsModal
    
    * Add none selected state for draft goals
    
    * (WIP) Fix styling in draft goals modal
    
    * Undo previous change
    
    * Remove none selected state from EditingMarksModal
    
    * WRITE-1442: Finish up styling of draft goals modal.
    
    * WRITE-1442: tweaking mlmenulist styles.
    
    * WRITE-1442: final cleanups, adding more goals and removing old/unused classes.
    
    * WRITE-1440: finish up styling and minor refactoring of modals to make them work as designed.
    
    * WRITE-1440: rename event to e.
    
    Approved-by: Abby <abby.delossantos.contractor@macmillan.com>
    Approved-by: erik swedberg <erik@erikswedberg.com>

--- frontend/app/src/components/FeedbackTool/CommentModal/EditingMarksModal.js
+++ frontend/app/src/components/FeedbackTool/CommentModal/EditingMarksModal.js
@@ -1,101 +1,117 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { MLButton, MLTag } from '../../MLComponents';
-import { MLMenuList } from '../../MLComponents';
-import { CommentLevelButtons } from '../index.js';
+import { MLButton, MLMenuList } from '../../MLComponents';
 import styles from './commentModal.css';
 
-class OpenCommentModal extends Component {
+class EditingMarksModal extends Component {
   state = {
     comment: null,
-    level: null
+    editingMark: null
   };
 
-  handleChange = event => {
-    this.setState({ comment: event.target.textContent.trim() });
+  dummyEditingMarks = [
+    { title: 'Comma splice', id: '1' },
+    { title: 'Fragment', id: '2' },
+    { title: 'Usage', id: '3' },
+    { title: 'Pronoun Agreement', id: '4' },
+    { title: 'Subject-verb Agreement', id: '5' },
+    { title: 'Appropriate Language', id: '6' },
+    { title: 'Needs Analysis', id: '7' },
+    { title: 'Comma Error', id: '8' },
+    { title: 'Apostrophe Error', id: '9' },
+    { title: 'Integrate Source (MLA)', id: '10' },
+    { title: 'Integrate Souce (APA)', id: '11' },
+    { title: 'Needs Evidence', id: '12' },
+    { title: 'Pronoun Reference', id: '13' },
+    { title: 'Quotation Marks', id: '14' },
+    { title: 'Spelling', id: '15' },
+    { title: 'Documentation (MLA)', id: '16' },
+    { title: 'Documentation (APA)', id: '17' },
+    { title: 'Verb Error', id: '18' }
+  ];
+
+  handleEditingMarkChange = e => {
+    this.setState({ editingMark: e.target.textContent });
   };
 
-  handleBackgroundClick = event => {
-    if (event.target === event.currentTarget) {
-      this.props.closeModal();
-    }
-  };
-
-  handleLevelClick = (e) => {
-    this.setState({
-      level: e.target.innerText
-    });
-  };
-
-  deleteTag = (e) => {
-    e.preventDefault();
-    this.setState({
-      level: null
-    });
+  handleCommentChange = e => {
+    this.setState({ comment: e.target.textContent.trim() });
   };
 
   render() {
     return (
       <div>
-        {this.props.modalType !== 'editingMarks'
-          ? <CommentLevelButtons
-            level={this.state.level}
-            handleLevelClick={this.handleLevelClick}
-            />
-          : null
-        }
-
-        <div className={styles.comments}>
-          <div
-            className={styles.commentTextWrapper}
-          >
-            {this.state.level && this.props.modalType !== 'editingMarks'
-              ? <MLTag text={this.state.level} deleteTag={this.deleteTag} />
-              : null
-            }
-            <div
-              placeholder="Please leave additional feedback here"
-              contentEditable={true}
-              suppressContentEditableWarning={true}
-              onKeyUp={this.handleChange}
-              className={styles.commentText}
-            />
-          </div>
+        <div className={styles.header}>
+          Editing Marks
         </div>
 
-        <div className={styles.controls}>
-          {(this.props.createFeedbackError && this.props.createFeedbackError.status)
-            ? <div className={styles.feedbackError}>
-                There was a problem saving your comment, please try again.
+        <div className={styles.modalWrapper}>
+          <MLMenuList list={this.dummyEditingMarks} callback={this.handleEditingMarkChange} />
+
+          <div className={styles.rightPanel}>
+            <div className={styles.commentWrapper}>
+
+              <div className={styles.section}>
+                <div className={styles.sectionHeader}>
+                  <div className={styles.sectionHeaderText}>Automatic Comment</div>
+                  <div className={styles.commentsHeadingLine} />
+                </div>
+                <div className={styles.commentDescription}>
+                  A comma splice occurs when two independent clauses are connected by only a comma. Independent clauses
+                  can stand by themselves as sentences, but they can't be connected with just a comma.
+                </div>
               </div>
-            : null
-          }
-          <MLButton
-            className={styles.addCommentButton}
-            dataId="cancel-comment-modal"
-            title="Cancel"
-            color="red"
-            bordered={true}
-            handleClick={this.props.closeModal}
-          />
-          <MLButton
-            className={styles.addCommentButton}
-            dataId="save-comment-modal"
-            title="Save"
-            handleClick={this.props.handleSave.bind(this, this.state.comment, this.state.level)}
-            disabled={!this.state.level}
-          />
+
+              <div className={styles.section}>
+                <div className={styles.sectionHeader}>
+                  <div className={styles.sectionHeaderText}>Additional Comment</div>
+                  <div className={styles.commentsHeadingLine} />
+                </div>
+                <div className={styles.commentTextWrapper}>
+                  <div
+                    placeholder="(Optional)"
+                    contentEditable={true}
+                    suppressContentEditableWarning={true}
+                    onKeyUp={this.handleCommentChange}
+                    className={styles.commentText}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.controls}>
+              {this.props.createFeedbackError && this.props.createFeedbackError.status
+                ? <div className={styles.feedbackError}>
+                  There was a problem saving your comment, please try again.
+                </div>
+                : null}
+              <MLButton
+                className={styles.addCommentButton}
+                dataId="cancel-comment-modal"
+                title="Cancel"
+                color="red"
+                bordered={true}
+                handleClick={this.props.closeModal}
+              />
+              <MLButton
+                className={styles.addCommentButton}
+                dataId="save-comment-modal"
+                title="Save"
+                // handleClick={this.props.handleSave.bind(this, this.state.comment)}
+                disabled={!this.state.editingMark}
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-OpenCommentModal.propTypes = {
+EditingMarksModal.propTypes = {
   closeModal: PropTypes.func,
   handleSave: PropTypes.func,
-  createFeedbackError: PropTypes.object,
-  modalType: PropTypes.string
+  createFeedbackError: PropTypes.object
 };
 
-export default OpenCommentModal;
+export default EditingMarksModal;
