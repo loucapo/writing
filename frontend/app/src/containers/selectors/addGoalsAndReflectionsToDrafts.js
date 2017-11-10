399c0136424dc88aa4a1b51b40d7b93872351a2f
Abby <abby.delossantos.contractor@macmillan.com>
2017-11-10 13:25:58 -0500

    fix merge conflicts

--- frontend/app/src/containers/selectors/addGoalsAndReflectionsToDrafts.js
+++ frontend/app/src/containers/selectors/addGoalsAndReflectionsToDrafts.js
@@ -1,10 +1,6 @@
 export default state => {
-<<<<<<< HEAD
-  const activityId = (state.activities[0] && state.activities[0].activityId) || (state.auth.activity && state.auth.activity.activityId);
-=======
   const activity = state.activities[0];
   const activityId = (activity && activity.activityId) || (state.auth.activity && state.auth.activity.activityId);
->>>>>>> cb9f44d4ec802d5b85431a3bc910fd064c921af9
 
   return state.drafts
     .filter(draft => draft.activityId === activityId)
